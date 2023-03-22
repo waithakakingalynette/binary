4f2b677977e3ea23fba41f38b82c74eb844e18ae
waithakakingalynette <warukiralynette@gmail.com>
2023-03-22 16:06:41 +0300

    commit

--- binary.js
+++ binary.js
@@ -2,34 +2,34 @@
 // the target exists in the array. If the target is not found, return null
 let num = [45,12,6,89,2,5]
   let target = 6;
-let firstIndex = 0;
-let lastIndex = num.length-1;
-let middleIndex = Math.floor((firstIndex+lastIndex)/2);
-while(num[middleIndex]!==firstIndex<=lastIndex){
-    console.log(middleIndex);
-    console.log(firstIndex);
-    console.log(lastIndex);
-    if(num[middleIndex]>6){
-        lastIndex=middleIndex-1;
+let first= 0;
+let last = num.length-1;
+let middle = Math.floor((first+last)/2);
+while(num[middle]!==first<=last){
+    console.log(middle);
+    console.log(first);
+    console.log(last);
+    if(num[middle]>6){
+        last=middle-1;
     }  else{
-        lastIndex=middleIndex+1;
+        last=middle+1;
     }
-    middleIndex=Math.floor((firstIndex+lastIndex)/2);{
+    middle=Math.floor((first+last)/2);{
     }
-    return num[middleIndex]===lastIndex-1;
+    return num[middle]===last-1;
     }
  
     //Given an unsorted array of numbers return the sorted array in descending order
     let arr=[123,89,5,23,9,56]
     function descending (arr,merge){
-        let leftIndex = 0;
-        let rightIndex = length-1;
-        while(leftIndex.length&&rightIndex.length){
-            if(leftIndex [0]<rightIndex[0]);{
-            arr.push(leftIndex.shift());
+        let left = 0;
+        let right = length-1;
+        while(left.length&&right.length){
+            if(left [0]<right[0]);{
+            arr.push(left.shift());
         }
     }
-    return[...arr ,...leftIndex,...rightIndex];
+    return[...arr ,...left,...right];
     }
     function mergeSort(unsortedArray){
         const middleIndex = unsortedArray.length/2;
